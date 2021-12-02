9387b2659ae96cc5f965ca9b731d623f57f7fb7e
David King <davidking41998@gmail.com>
2021-12-02 10:42:33 +0100

    day 2

---
+++ day2.py
@@ -0,0 +1,30 @@
+from sys import stdin
+
+inst = [x.split() for x in stdin]
+
+
+def go(inst, part):
+    horiz, depth, aim = 0, 0, 0
+    for x, n in inst:
+        if part == 1:
+            match x:
+                case "forward":
+                    horiz += int(n)
+                case "down":
+                    depth += int(n)
+                case "up":
+                    depth -= int(n)
+        if part == 2:
+            match x:
+                case "forward":
+                    horiz += int(n)
+                    depth += int(n)*aim
+                case "down":
+                    aim += int(n)
+                case "up":
+                    aim -= int(n)
+    return(horiz * depth)
+
+
+print(go(inst, part = 1))
+print(go(inst, part = 2))
